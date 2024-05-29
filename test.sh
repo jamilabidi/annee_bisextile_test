1fcac66f0ae2a005ccb22cf8db266f0e4371f889
jamil abidi <jamil.abidi@le-campus-numerique.fr>
2024-05-29 12:09:30 +0200

    1rst try with pytest via shell script

---
+++ test.sh
@@ -0,0 +1,9 @@
+pytest test_leap_year.py > status.txt
+if [[  $(grep -i status.txt) == "" ]]
+then
+  echo "les tests sont passés!"
+  python leap_year.py
+else
+  echo "ERROR in TEST"
+  cat status.txt
+fi
